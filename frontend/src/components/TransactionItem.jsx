3ff627011b790b7b7d2fa4b444db4f7af33bd31e
Disha <dg.mechpro200@gmail.com>
2026-04-26 14:11:46 +0530

    Update: 2026-04-26 14:11:43

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { transactionItemStyles } from '../assets/dummyStyles'
 import { colorClasses } from '../assets/color'
 import { Save, X, Edit, Trash2, DollarSign } from 'lucide-react';
